5bfd80ec9fdd8993d03ad85daa69ffb52e438f46
Raphael Okere <raphaelokere@Raphaels-MacBook-Pro.local>
2025-01-05 15:43:10 -0500

    test 3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,25 @@
 provider "google" {
   project = var.project_id
-  region  = "us-east"
+  region  = "us-east"  # Example region
 }
 
 resource "google_container_cluster" "primary" {
-  name               = "qdrant-cluster"
+  name               = "my-gke-cluster"
   location           = "us-east"
   initial_node_count = 3
 
   node_config {
     machine_type = "e2-standard-4"
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
+
+  # Enable Workload Identity
+  workload_identity_config {
+    workload_pool = "${var.project_id}.svc.id.goog"
+  }
+}
+
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
 }
